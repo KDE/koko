1b46dca59bde96b8487755f7f54b86249ca95a86
Vishesh Handa <me@vhanda.in>
2015-02-09 15:38:57 +0100

    cmake finder: kdetree -> kdtree
    Also mark it as REQURIED

--- cmake/FindKdTree.cmake
+++ cmake/FindKdTree.cmake
@@ -8,4 +8,4 @@ find_path (KDTREE_INCLUDE_DIR NAMES kdtree.h)
 find_library (KDTREE_LIBRARIES NAMES kdtree)
 
 include (FindPackageHandleStandardArgs)
-find_package_handle_standard_args (KdTree DEFAULT_MSG KDETREE_LIBRARIES KDETREE_INCLUDE_DIR)
+find_package_handle_standard_args (KdTree DEFAULT_MSG KDTREE_LIBRARIES KDTREE_INCLUDE_DIR)
