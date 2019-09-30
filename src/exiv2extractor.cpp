e991e94ebe280bf5adae4843f6da31ebceaebd44
Nicolas Fella <nicolas.fella@gmx.de>
2019-09-30 13:21:35 +0200

    Fix build against exiv for real
    Summary: Current master is not 0.28 yet
    
    Test Plan: Now really builds
    
    Reviewers: mart, davidedmundson
    
    Reviewed By: davidedmundson
    
    Differential Revision: https://phabricator.kde.org/D24173

--- src/exiv2extractor.cpp
+++ src/exiv2extractor.cpp
@@ -127,7 +127,7 @@ void Exiv2Extractor::extract(const QString& filePath)
     std::string fileString(arr.data(), arr.length());
 
     Exiv2::LogMsg::setLevel(Exiv2::LogMsg::mute);
-#if EXIV2_TEST_VERSION(0, 28, 0)
+#if EXIV2_TEST_VERSION(0, 27, 99)
     Exiv2::Image::UniquePtr image;
 #else
      Exiv2::Image::AutoPtr image;
