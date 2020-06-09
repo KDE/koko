ae10e050c38f8573c35288fc77b2f1618bce3073
Carl Schwan <carl@carlschwan.eu>
2020-06-09 17:59:32 +0000

    Port to reuse

--- src/imagetimemodel.h
+++ src/imagetimemodel.h
@@ -1,21 +1,8 @@
 /*
- * Copyright (C) 2017 Atul Sharma <atulsharma406@gmail.com>
- * Copyright (C) 2014  Vishesh Handa <me@vhanda.in>
+ * SPDX-FileCopyrightText: (C) 2014 Vishesh Handa <vhanda@kde.org>
+ * SPDX-FileCopyrightText: (C) 2017 Atul Sharma <atulsharma406@gmail.com>
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- *
+ * SPDX-LicenseIdentifier: LGPL-2.1-or-later
  */
 
 #ifndef IMAGETIMEMODEL_H
