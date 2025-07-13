5bedcc94e40e9f25f23c57ff6ff566045ff01594
Carl Schwan <carl@carlschwan.eu>
2025-07-13 12:18:37 +0200

    Use qt extension based mimetype detection
    Instead of hardcoding a list of know image extension, use Qt extension based
    mimetype detection.

--- src/filesystemimagefetcher.cpp
+++ src/filesystemimagefetcher.cpp
@@ -28,13 +28,6 @@ void FileSystemImageFetcher::slotProcess()
 {
     static QMimeDatabase mimeDb;
 
-    static constexpr auto allowedExtensions = std::to_array<QLatin1StringView>({
-        "png"_L1,
-        "jpg"_L1,
-        "jpeg"_L1,
-        "heic"_L1,
-    });
-
     QDirIterator it(m_folder, QDirIterator::Subdirectories);
     while (it.hasNext()) {
         const QString filePath = it.next();
@@ -44,21 +37,11 @@ void FileSystemImageFetcher::slotProcess()
             continue;
         }
 
-        const auto extension = QStringView(filePath).split('.').constLast();
-        if (std::ranges::find_if(allowedExtensions,
-                                 [&extension](const QLatin1StringView &allowedExtension) -> bool {
-                                     return allowedExtension == extension;
-                                 })
-            != allowedExtensions.cend()) {
+        static QMimeDatabase db;
+        const auto mimetype = db.mimeTypeForFile(filePath, QMimeDatabase::MatchMode::MatchExtension).name();
+        if (mimetype.startsWith("image/"_L1) || mimetype.startsWith("video/"_L1)) {
             Q_EMIT imageResult(filePath);
-            return;
         }
-
-        const QString mimetype = mimeDb.mimeTypeForFile(filePath).name();
-        if (!mimetype.startsWith("image/"_L1) && !mimetype.startsWith("video/"_L1))
-            continue;
-
-        Q_EMIT imageResult(filePath);
     }
 
     Q_EMIT finished();
