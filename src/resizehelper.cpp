db721b9bdc342d5073caf8c8c16c235b5c770c6f
Noah Davis <noahadvs@gmail.com>
2026-03-11 05:00:34 -0400

    Add estimated size to image resize popup

---
+++ src/resizehelper.cpp
@@ -0,0 +1,30 @@
+/* SPDX-FileCopyrightText: 2026 Noah Davis <noahadvs@gmail.com>
+ * SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+
+#include "resizehelper.h"
+
+#include <QBuffer>
+#include <QImageWriter>
+#include <KIO/Global>
+
+ResizeHelper::ResizeHelper(QObject *parent)
+    : QObject(parent)
+{
+}
+
+QString ResizeHelper::fileSize(AnnotationDocument *doc, int width, int height, const QString &mimeType)
+{
+    if (!doc || width < 1 || height < 1) {
+        return {};
+    }
+    auto image = doc->canvasBaseImage();
+    QBuffer buffer;
+    buffer.open(QIODevice::ReadWrite);
+    // TODO: Find a cheaper way to estimate resized large images that is still fairly accurate and reliable.
+    image = image.scaled(width, height, Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
+    image.save(&buffer, QImageWriter::imageFormatsForMimeType(mimeType.toLatin1()).value(0));
+    qint64 size = buffer.size();
+    buffer.close();
+    return KIO::convertSize(size);
+}
