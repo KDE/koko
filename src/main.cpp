2c91b01168daaae2ceb7b6f9ce7eae39c98b7e89
Marco Martin <notmart@gmail.com>
2026-04-01 09:18:52 +0200

    Rename FileMenuActions to FileMenuManager
    the actions are not instantiated there anymore, we only connect and
    then execute the logic on triggered

--- src/main.cpp
+++ src/main.cpp
@@ -30,7 +30,7 @@
 #include <iostream>
 
 #include "checkerboardimageprovider.h"
-#include "filemenuactions.h"
+#include "filemenumanager.h"
 #include "filesystemtracker.h"
 #include "galleryopenmodel.h"
 #include "imagestorage.h"
